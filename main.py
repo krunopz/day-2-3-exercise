0b0f0598ad70b1ea5e5ddad53502d6c7c8b349b2
Krunoslav Stefanek <prof.stefanek@gmail.com>
2021-01-07 14:38:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+#Let's say that we live for 90 years. Year has 365 days, 52 weeks and #12 months.
+#Write your code below this line 👇
+years=int(age);
+lleft=90-years;
+days=lleft*365;
+weeks=lleft*52;
+months=lleft*12;
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
+
+
+
+
+
+
+
+
